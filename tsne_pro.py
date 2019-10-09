630e38e5fbf93b345f01b281a5d4454934509c45
SZJhacker <shenzijie2013@163.com>
2019-10-09 15:19:57 +0800

    "format normalization"

--- tsne_pro.py
+++ tsne_pro.py
@@ -11,8 +11,8 @@ import argparse
 def tsne_visual(pltdata):
     """对t-SNE降维的数据进行可视化"""
 
-    p1 = pltdata[(pltdata.label=="positive")]
-    p2 = pltdata[(pltdata.label=="negative")]
+    p1 = pltdata[(pltdata.label == "positive")]
+    p2 = pltdata[(pltdata.label == "negative")]
     x1 = p1.values[:, 0]
     y1 = p1.values[:, 1]
     x2 = p2.values[:, 0]
@@ -25,51 +25,75 @@ def tsne_visual(pltdata):
     plt.ylabel('Dimension2', fontsize=9)
     plt.margins(0.3)
     plt.legend(loc="upper right")
-    
+
 
 def tsne_data(rawdata):
     """对输入数据进行处理降维并返回降维结果"""
     modle = TSNE(n_components=2, random_state=0)
 
     fea_data = rawdata.drop(columns=['class'])  #取出所有特征向量用于降维
-    redu_fea = modle.fit_transform(fea_data) #将数据降到2维进行后期的可视化处理
-    labels = rawdata['class'].replace([0, 1], ['negative', 'positive']) #将正反例数字替换成正反例标签用于收起展示
-    redu_data = np.vstack((redu_fea.T, labels.T)).T #将特征向量和正反例标签整合
-    tsne_df = pd.DataFrame(data=redu_data, columns=['Dimension1', 'Dimension2', "label"])
+    redu_fea = modle.fit_transform(fea_data)  #将数据降到2维进行后期的可视化处理
+    labels = rawdata['class'].replace(
+        [0, 1], ['negative', 'positive'])  #将正反例数字替换成正反例标签用于收起展示
+    redu_data = np.vstack((redu_fea.T, labels.T)).T  #将特征向量和正反例标签整合
+    tsne_df = pd.DataFrame(
+        data=redu_data, columns=['Dimension1', 'Dimension2', "label"])
     tsne_visual(tsne_df)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Visualzes the features based on t-sne in a 2D feature space")
-    parser.add_argument('-i', '--infile', help="The input file should be csv format, and multiple file should be separated by commas", required=True)
-    parser.add_argument('-o', '--outfile', help='The name of output picture', required=True)
-    parser.add_argument('-l', '--layout', help='The layout of subplots, please input row,column number, default 1,1 ', default='1,1')
-    parser.add_argument('-d', '--dpi', help='The dpi of output picture, default is 300dpi', default=300)
+    parser = argparse.ArgumentParser(
+        description=
+        "Visualzes the features based on t-sne in a 2D feature space")
+    parser.add_argument(
+        '-i',
+        '--infile',
+        help=
+        "The input file should be csv format, and multiple file should be separated by commas",
+        required=True)
+    parser.add_argument(
+        '-o', '--outfile', help='The name of output picture', required=True)
+    parser.add_argument(
+        '-l',
+        '--layout',
+        help=
+        'The layout of subplots, please input row,column number, default 1,1 ',
+        default='1,1')
+    parser.add_argument(
+        '-d',
+        '--dpi',
+        help='The dpi of output picture, default is 300dpi',
+        default=300)
     args = parser.parse_args()
 
-    infile_list = args.infile.split(',') # 获得文件名列表
+    infile_list = args.infile.split(',')  # 获得文件名列表
     # title = [i.replace(r'.csv', '') for i in infile_list] # 以文件名为图片子标题列表
-    row, column = [int(i) for i in args.layout.split(',')] 
-    assert len(infile_list) <= row*column, "The number of inputfile to be drawn is not greater than the number of subplots of the canvas, please set '-l' parameter"
+    row, column = [int(i) for i in args.layout.split(',')]
+    assert len(
+        infile_list
+    ) <= row * column, "The number of inputfile to be drawn is not greater than the number of subplots of the canvas, please set '-l' parameter"
 
     # if numb > 1:
     #     for i in range(numb):
     #         plt.subplot(row, column, i+1)
-    #         plt.subplots_adjust(wspace =0.3, hspace =0.35) #调节子图之间的宽度 
+    #         plt.subplots_adjust(wspace =0.3, hspace =0.35) #调节子图之间的宽度
     #         process(infile_list[i])
     # else:
     #     process(infile_list[0])
-    
+
     # 通过循环对各个文件进行处理，使用数字索引是为了确定子画布的位置
-    plt.figure(figsize=(column*4.7, row*3)) #设置画布的尺寸
+    plt.figure(figsize=(column * 4.7, row * 3))  #设置画布的尺寸
 
     for i in range(len(infile_list)):
-        plt.subplot(row, column, i+1)
+        plt.subplot(row, column, i + 1)
         csvdata = pd.read_csv(infile_list[i])
         tsne_data(csvdata)
 
-    plt.subplots_adjust(top=0.92, bottom=0.20, left=0.15, right=0.95, hspace=0.20, wspace=0.20) # 调整子图之间的分布
-    plt.savefig(args.outfile, dpi=args.dpi)    
+    plt.subplots_adjust(
+        top=0.92, bottom=0.20, left=0.15, right=0.95, hspace=0.20,
+        wspace=0.20)  # 调整子图之间的分布
+    plt.savefig(args.outfile, dpi=args.dpi)
+
 
 if __name__ == "__main__":
     main()
